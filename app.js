985d1afae67dc1a97f1e63c2da815acee5f746cc
Tobias Viana <eutobias@gmail.com>
2022-02-10 19:38:42 -0300

    another branch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World, FROM BRANCH 1!");
+  res.send("Hello World, FROM BRANCH 2!");
 });
 
 app.use(function (err, req, res, next) {
