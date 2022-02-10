b1fd83fbeb0d38af5a70eec1f7e02dff51260712
Tobias Viana <eutobias@gmail.com>
2022-02-10 19:35:40 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).json({
+      message: err.message,
+      stack: err.stack
+  });
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
